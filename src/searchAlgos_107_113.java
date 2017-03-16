4a7191f156918aa9186eace50832acfe047e8510
vrr-21 <>
2017-03-16 16:15:43 +0530

    First Change from Macbook. Comment removed.

--- src/searchAlgos_107_113.java
+++ src/searchAlgos_107_113.java
@@ -11,75 +11,7 @@ public class searchAlgos_107_113{
                 
                 
                 bfs(map);
-		/*
-		do
-		{ 
-		 printf("1.Up(Towards %s)\n",station_names[0]);
-		 printf("2.Down(Towards %s)",station_names[nos-1]);
-		 printf("\n3.Display Map Representation");
-		 printf("\n0.Exit\nEnter your choice:");
-		 scanf("%d",&ch);
-		 switch(ch)
-		 {
-		   case 1:display_stations();
-			      printf("Enter source: ");
-			      scanf("%d",&source);
-			      printf("Enter destination:");
-			      scanf("%d",&dest);
-			      dfs(up,source,dest);
-			      break;
-		   case 2:display_stations();
-			      printf("Enter source: ");
-			      scanf("%d",&source);
-			      printf("Enter destination:");
-			      scanf("%d",&dest);
-			      dfs(down,source,dest);
-			      break;
-		   case 3:bfs(down,0);
-			      break;
-		 }
-		}while(ch!=0);*/
-		
-		
-	}
-	
-	/*void create_links()
-	{
-	  int i,j,n,station;
-	  for(i=0;i<nos;i++)
-	  {
-	    printf("Enter number of immediately next stations of %s:",station_names[i]);
-	    scanf("%d",&n);
-	    if(n!=0)
-	    {
-	      display_stations();
-		printf("Enter corresponding station numbers:\n");
-	      for(j=0;j<n;j++)
-	      {
-		scanf("%d",&station);
-		down[i][station]=1;
-		up[station][i]=1;
-	      }
-	    }
-	  }
-	}
-	static void BFS(int list[][MAX],int source,int dest)
-	{
-            
-	}
-	void display_stations()
-	{
-	  int j;
-	  for(j=0;j<nos;j++)
-	    printf("%d. %s\n",j,station_names[j]);
-	}
-	void display_list(int stack[],int top)
-	{
-	  int i;
-	  for(i=0;i<top;i++)
-	    printf("%s->",station_names[stack[i]]);
-	  printf("%s\n",station_names[stack[top]]);
-	}*/
+        }
 	
         static void bfs(MapOfRomania map)
         {
